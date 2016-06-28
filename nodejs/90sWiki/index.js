74a524b1befac3f65308df853a5fbafd6047193d
Chappie <phamous2day@gmail.com>
2016-06-28 11:35:57 -0400

    Uploaded 3rd iteration of Wiki, now with login and redirect to page visited

--- nodejs/90sWiki/index.js
+++ nodejs/90sWiki/index.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var session = require('express-session');
 var wikiLinkify = require('wiki-linkify');
 var app = express();
 var marked = require('marked');
 
+app.use(session({
+  secret: 'magiczebra',
+  cookie: {
+    maxAge: 24 * 60 * 60000
+  }
+}));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
-
   response.redirect('/HomePage');
 });
 
-app.get('/:pageName', function(request, response) {
+app.get('/login', function(request, response) {
+  response.render('login.hbs', {
+    title: 'Login'
+  });
+});
 
+app.get('/logout', function(request, response) {
+  request.session.user = null;
+  response.redirect('/');
+});
+
+app.post('/login-submit', function(request, response) {
+  var credentials = request.body;
+  if (credentials.username === 'Toby' &&
+      credentials.password === 'abc') {
+    request.session.user = credentials.username;
+    response.redirect(request.session.requestUrl);
+  } else {
+
+    response.redirect('/');
+  }
+});
+
+app.get('/:pageName', function(request, response) {
   var pageName = request.params.pageName;
   var filename = 'pages/' + pageName + '.txt';
+  var user = request.session.user;
   fs.access(filename, fs.R_OK, function(err) {
     if (err) {
       // cannot read, rendering place holder page
@@ -35,7 +64,8 @@ app.get('/:pageName', function(request, response) {
         response.render('page.hbs', {
           title: pageName,
           content: marked(wikiContent),
-          pageName: pageName
+          pageName: pageName,
+          user: user
         });
       });
     }
@@ -43,7 +73,16 @@ app.get('/:pageName', function(request, response) {
 
 });
 
-app.get('/:pageName/edit', function(request, response) {
+function authRequired(request, response, next) {
+  if (!request.session.user) {
+    request.session.requestUrl = request.url;
+    response.redirect('/login');
+  } else {
+    next();
+  }
+}
+
+app.get('/:pageName/edit', authRequired, function(request, response) {
   var pageName = request.params.pageName;
   var filename = 'pages/' + pageName + '.txt';
   fs.readFile(filename, function(err, buffer) {
@@ -63,7 +102,7 @@ app.get('/:pageName/edit', function(request, response) {
 
 });
 
-app.post('/:pageName/save', function(request, response) {
+app.post('/:pageName/save', authRequired, function(request, response) {
   var pageName = request.params.pageName;
   var content = request.body.content;
   var filename = 'pages/' + pageName + '.txt';
