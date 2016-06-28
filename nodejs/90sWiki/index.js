7ea8824af43f8004ca9da4f1534573a12cabdc89
Chappie <phamous2day@gmail.com>
2016-06-28 08:50:26 -0400

    Uploading functionality to edit pages

--- nodejs/90sWiki/index.js
+++ nodejs/90sWiki/index.js
@@ -1,49 +1,66 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var wikiLinkify = require('wiki-linkify');
 var app = express();
+var marked = require('marked');
 
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response) {
+
   response.redirect('/HomePage');
 });
 
 app.get('/:pageName', function(request, response) {
+
   var pageName = request.params.pageName;
   var filename = 'pages/' + pageName + '.txt';
-  fs.access(filename, fs.R_OK, function (err) {
+  fs.access(filename, fs.R_OK, function(err) {
     if (err) {
+      // cannot read, rendering place holder page
       response.render('placeholder.hbs', {
-        title:pageName
+        title: pageName
       });
     } else {
-      fs.readFile(filename, function (err, buffer){
+      fs.readFile(filename, function(err, buffer) {
         if (err) {
           response.statusCode = 500;
-          response.send("sorry, problem reading file");
+          response.send('Sorry, problem reading file.');
           return;
         }
         var content = buffer.toString();
-        response.render(page.hbs {
+        var wikiContent = wikiLinkify(content);
+        response.render('page.hbs', {
           title: pageName,
-          content: content
-        }) ;
+          content: marked(wikiContent),
+          pageName: pageName
+        });
       });
     }
   });
-  response.render('placeholder.hbs', {
-    title: pageName
-  });
+
 });
 
 app.get('/:pageName/edit', function(request, response) {
   var pageName = request.params.pageName;
-  response.render('edit.hbs', {
-    title: 'Edit ' + pageName,
-    pageName: pageName
+  var filename = 'pages/' + pageName + '.txt';
+  fs.readFile(filename, function(err, buffer) {
+    var content;
+    if (err) {
+      content = '';
+    } else {
+      content = buffer.toString();
+    }
+
+    response.render('edit.hbs', {
+      title: 'Edit ' + pageName,
+      pageName: pageName,
+      content: content
+    });
   });
+
 });
 
 app.post('/:pageName/save', function(request, response) {
